08de6e0adf24a494dc45a8d3675b31c4cce3012b
Ekaterina <kat.bommon@gmail.com>
2023-02-16 02:07:40 +0300

    out sum recursion

---
+++ task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int OutNumber(int n, int m, int summ)
+{
+    if (m <= n)
+    {
+
+        summ = m + OutNumber(n, m + 1, summ);
+    }
+    return summ;
+}
+Console.Write(OutNumber(8, 4, 0));
